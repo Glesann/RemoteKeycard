71eef42b721144df5ec91932bf6000c665c50abf
Glesan <67572767+Glesann@users.noreply.github.com>
2025-04-28 17:47:51 +0800

    Support Exiled-9.6.0
    fix

---
+++ Config.cs
@@ -0,0 +1,59 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteKeycard
+{
+    public class Config : IConfig
+    {
+        /// <summary>
+        ///     Whether Amnesia affects the usage of keycards.
+        /// </summary>
+        [Description("Whether  Amnesia affects the usage of keycards.")]
+        public bool AmnesiaMatters { get; set; } = true;
+
+        /// <summary>
+        ///     Whether this plugin works on generators.
+        /// </summary>
+        [Description("Whether this plugin works on generators.")]
+        public bool AffectGenerators { get; set; } = true;
+
+        /// <summary>
+        ///     Whether this plugin works on Warhead's panel.
+        /// </summary>
+        [Description("Whether this plugin works on Warhead's panel.")]
+        public bool AffectWarheadPanel { get; set; } = true;
+
+        /// <summary>
+        ///     Whether this plugin works on SCP lockers.
+        /// </summary>
+        [Description("Whether this plugin works on SCP lockers.")]
+        public bool AffectScpLockers { get; set; } = true;
+
+        /// <summary>
+        ///     Whether this plugin works on doors.
+        /// </summary>
+        [Description("Whether this plugin works on doors.")]
+        public bool AffectDoors { get; set; } = true;
+
+        /// <summary>
+        ///     Gets whether exceptions should be shown.
+        /// </summary>
+        [Description("Toggle on/off exceptions/errors in console. (Enable this before reporting ANY bugs)")]
+        public bool ShowExceptions { get; set; } = false;
+
+        /// <summary>
+        ///    Gets or sets a value indicating whether this plugin is enabled.
+        /// </summary>
+        public bool IsEnabled { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether debug messages should be shown.
+        /// </summary>
+        public bool Debug { get; set; } = false;
+    }
+}
